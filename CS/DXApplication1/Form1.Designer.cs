651b214e69edf6e430bd3d3ed3d3bf53e672c02e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXApplication1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace DXApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.videoRentDataSet = new DXApplication1.VideoRentDataSet();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colOid = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colCustomerId = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colMiddleName = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colEmail = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAddress = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colPhone = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colComments = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colPhoto = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colDiscountLevel = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.customerTableAdapter = new DXApplication1.VideoRentDataSetTableAdapters.CustomerTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoRentDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.customerBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(433, 334);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // customerBindingSource
+            // 
+            this.customerBindingSource.DataMember = "Customer";
+            this.customerBindingSource.DataSource = this.videoRentDataSet;
+            // 
+            // videoRentDataSet
+            // 
+            this.videoRentDataSet.DataSetName = "VideoRentDataSet";
+            this.videoRentDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colOid,
+            this.colCustomerId,
+            this.colMiddleName,
+            this.colEmail,
+            this.colAddress,
+            this.colPhone,
+            this.colComments,
+            this.colPhoto,
+            this.colDiscountLevel});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.GroupCount = 1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.SortInfo.AddRange(new DevExpress.XtraGrid.Columns.GridColumnSortInfo[] {
+            new DevExpress.XtraGrid.Columns.GridColumnSortInfo(this.colDiscountLevel, DevExpress.Data.ColumnSortOrder.Ascending)});
+            this.gridView1.CustomDrawGroupRow += new DevExpress.XtraGrid.Views.Base.RowObjectCustomDrawEventHandler(this.gridView1_CustomDrawGroupRow);
+            // 
+            // colOid
+            // 
+            this.colOid.FieldName = "Oid";
+            this.colOid.Name = "colOid";
+            this.colOid.Visible = true;
+            this.colOid.VisibleIndex = 0;
+            // 
+            // colCustomerId
+            // 
+            this.colCustomerId.FieldName = "CustomerId";
+            this.colCustomerId.Name = "colCustomerId";
+            this.colCustomerId.Visible = true;
+            this.colCustomerId.VisibleIndex = 1;
+            // 
+            // colMiddleName
+            // 
+            this.colMiddleName.FieldName = "MiddleName";
+            this.colMiddleName.Name = "colMiddleName";
+            this.colMiddleName.Visible = true;
+            this.colMiddleName.VisibleIndex = 2;
+            // 
+            // colEmail
+            // 
+            this.colEmail.FieldName = "Email";
+            this.colEmail.Name = "colEmail";
+            this.colEmail.Visible = true;
+            this.colEmail.VisibleIndex = 3;
+            // 
+            // colAddress
+            // 
+            this.colAddress.FieldName = "Address";
+            this.colAddress.Name = "colAddress";
+            this.colAddress.Visible = true;
+            this.colAddress.VisibleIndex = 4;
+            // 
+            // colPhone
+            // 
+            this.colPhone.FieldName = "Phone";
+            this.colPhone.Name = "colPhone";
+            this.colPhone.Visible = true;
+            this.colPhone.VisibleIndex = 5;
+            // 
+            // colComments
+            // 
+            this.colComments.FieldName = "Comments";
+            this.colComments.Name = "colComments";
+            this.colComments.Visible = true;
+            this.colComments.VisibleIndex = 6;
+            // 
+            // colPhoto
+            // 
+            this.colPhoto.FieldName = "Photo";
+            this.colPhoto.Name = "colPhoto";
+            this.colPhoto.Visible = true;
+            this.colPhoto.VisibleIndex = 7;
+            // 
+            // colDiscountLevel
+            // 
+            this.colDiscountLevel.FieldName = "DiscountLevel";
+            this.colDiscountLevel.Name = "colDiscountLevel";
+            this.colDiscountLevel.Visible = true;
+            this.colDiscountLevel.VisibleIndex = 8;
+            // 
+            // customerTableAdapter
+            // 
+            this.customerTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 334);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.videoRentDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private VideoRentDataSet videoRentDataSet;
+        private System.Windows.Forms.BindingSource customerBindingSource;
+        private VideoRentDataSetTableAdapters.CustomerTableAdapter customerTableAdapter;
+        private DevExpress.XtraGrid.Columns.GridColumn colOid;
+        private DevExpress.XtraGrid.Columns.GridColumn colCustomerId;
+        private DevExpress.XtraGrid.Columns.GridColumn colMiddleName;
+        private DevExpress.XtraGrid.Columns.GridColumn colEmail;
+        private DevExpress.XtraGrid.Columns.GridColumn colAddress;
+        private DevExpress.XtraGrid.Columns.GridColumn colPhone;
+        private DevExpress.XtraGrid.Columns.GridColumn colComments;
+        private DevExpress.XtraGrid.Columns.GridColumn colPhoto;
+        private DevExpress.XtraGrid.Columns.GridColumn colDiscountLevel;
+
+    }
+}
+
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to customize information shown in the group row
+
+
+This example demonstrates how to customize the group row text. One of the ways to do this is to handle the <a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraGridViewsGridGridView_CustomDrawGroupRowtopic">GridView.CustomDrawGroupRow</a> event and set the <strong>GridGroupRowInfo.GroupText </strong>property to a required value.<br />See code in the Form.cs (Form.vb) file. 
+
+<br/>
+
+
